d573dbaf5f29dfe41bb8480eec1a9ef33c695860
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-18 18:06:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,10 @@ int main(int argc, char* argv[])
         server bulk_server(io_service, std::atoi(argv[1]), std::atoi(argv[2]));
         io_service.run();
         */
-            std::istringstream ss(argv[1]);
-    unsigned int port;
-    ss >> port;
-    if (port < 1 || port > 65535) {
-        std::cerr << "Порт должен быть числом от 1 до 65535" << std::endl;
-        return EXIT_FAILURE;
-    }
-    const size_t max_block_size = 1000000000;
-    size_t block_size;
-    ss = std::istringstream(argv[2]);
-    ss >> block_size;
-    if (block_size == 0 || block_size > max_block_size) {
-        std::cerr << "Размер блока должен быть целым, положительным числом, не более "
-                  << max_block_size << std::endl;
-        return EXIT_FAILURE;
-    }
-        server(port, block_size);
-    }
+   
+        server(std::atoi(argv[1]), std::atoi(argv[2]));
     
+    }
     catch (std::exception & e)
         
     {
