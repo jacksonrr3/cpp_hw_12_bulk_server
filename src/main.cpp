3091e9a508ee353af20eee0eb90dfb6a0e6a9056
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-18 15:20:57 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,9 @@ int main(int argc, char* argv[])
         io_service.run();
         
     }
+    
     catch (std::exception & e)
+        
     {
         std::cerr << "Exception: " << e.what() << "\n";
     }
