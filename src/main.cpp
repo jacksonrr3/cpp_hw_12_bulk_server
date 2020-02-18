f22e5c4038eed2f0cf86eccebbdf46d2bb5ef0a9
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-18 15:08:27 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "server.h"
+
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc < 2)
+        {
+            std::cerr << "Usage: chat_server <port> <bulk_size>\n";
+            return 1;
+        }
+       
+        ba::io_service io_service;
+        server bulk_server(io_service, std::atoi(argv[1]), std::atoi(argv[2]));
+        io_service.run();
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
