fb3dc85c9698d59504680df4b04094317ac111ce
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-19 15:59:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@ int main(int argc, char* argv[])
             std::cerr << "Usage: chat_server <port> <bulk_size>\n";
             return 1;
         }
-       /*
+       
         ba::io_service io_service;
         server bulk_server(io_service, std::atoi(argv[1]), std::atoi(argv[2]));
         io_service.run();
-        */
+        
    
-        server(std::atoi(argv[1]), std::atoi(argv[2]));
+      //  server(std::atoi(argv[1]), std::atoi(argv[2]));
     
     }
     catch (std::exception & e)
